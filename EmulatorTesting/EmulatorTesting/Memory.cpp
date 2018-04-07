dbf3562b6a422676cc2981520f7bb63bbe5e7b07
tntmeijs <tntmeijs@gmail.com>
2018-04-07 13:39:07 +0200

    Add basic memory class.

--- EmulatorTesting/EmulatorTesting/Memory.cpp
+++ EmulatorTesting/EmulatorTesting/Memory.cpp
@@ -1,1 +1,33 @@
 #include "Memory.hpp"
+
+Memory::Memory()
+	: MAX_ADDRESS(255)
+	, m_memory(new byte[MAX_ADDRESS])
+{
+	clearMemory();
+}
+
+Memory::~Memory()
+{
+	if (m_memory)
+	{
+		delete[] m_memory;
+		m_memory = nullptr;
+	}
+}
+
+void Memory::clearMemory()
+{
+	for (byte b = 0; b < MAX_ADDRESS; ++b)
+		m_memory[b] = 0;
+}
+
+const byte & Memory::read(const byte & p_address)
+{
+	return m_memory[p_address];
+}
+
+void Memory::write(const byte & p_address, const byte & p_value)
+{
+	m_memory[p_address] = p_value;
+}
--- EmulatorTesting/EmulatorTesting/Memory.hpp
+++ EmulatorTesting/EmulatorTesting/Memory.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+using byte = unsigned char;
+
 class Memory
 {
 public:
 	Memory();
 	~Memory();
 
+	// Resets every bit to zero
+	void clearMemory();
+
+	// Return the byte at this address
+	const byte & read(const byte & p_address);
+
+	// Set the byte at this address
+	void write(const byte & p_address, const byte & p_value);
+
+	// Maximum address we can read from
+	const byte MAX_ADDRESS;
+
 private:
+	// Memory to work with
+	byte * m_memory;
 };
